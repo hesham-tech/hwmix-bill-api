872cc0e2c7279411f5f6ffc9d44a1b86218bca24
hesham <atsh.phone@gmail.com>
2026-01-21 19:26:28 +0200

    Cleanup: remove temporary files and scripts

--- fix_transaction_types.sql
+++
@@ -1,4 +0,0 @@
-UPDATE transactions SET type = 'deposit' WHERE type = 'إيداع';
-UPDATE transactions SET type = 'withdraw' WHERE type = 'سحب';
-UPDATE transactions SET type = 'transfer_out' WHERE type IN ('تحويل_صادر', 'تحويل صادر');
-UPDATE transactions SET type = 'transfer_in' WHERE type IN ('تحويل_وارد', 'تحويل وارد');
